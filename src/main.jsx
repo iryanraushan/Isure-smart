c0d4eebfe9900ffca5a6ad18f218e1f356475a6b
Ryan Raushan <raushankrpnhs2000@gmail.com>
2024-06-28 09:33:38 +0530

    remove await

--- src/main.jsx
+++ src/main.jsx
@@ -5,8 +5,8 @@ import "./index.css";
 import { Auth0Provider } from "@auth0/auth0-react";
 import { ThemeProvider } from "@material-tailwind/react";
 
-const CLIENT_ID = await import.meta.env.VITE_CLIENT_ID;
-const DOMAIN_ID = await import.meta.env.VITE_DOMAIN_ID;
+const CLIENT_ID = import.meta.env.VITE_CLIENT_ID;
+const DOMAIN_ID = import.meta.env.VITE_DOMAIN_ID;
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
